155d517a9904709a9296f579329ea4539ab059c7
xenophon <674550338@qq.com>
2025-09-09 00:03:00 +0800

    feat: add toggle to control shadow following characters

---
+++ DanceShadowFollower.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class DanceShadowFollower : MonoBehaviour
+{
+    [Header("配置")]
+    public string shadowObjectName = "Shadow"; 
+
+    private Transform hips;         
+    private Transform shadowPlane;  
+    public float initialZOffset = 2.02f;   
+
+    public DanceAvatarHelper avatarHelper; 
+    public DancePlayerCore dancePlayerCore;
+
+    void Start()
+    {
+        if (avatarHelper != null)
+        {
+            var animator = avatarHelper.CurrentAnimator;
+            if (animator != null)
+            {
+                hips = animator.GetBoneTransform(HumanBodyBones.Hips);
+            }
+        }
+
+        GameObject shadowObj = GameObject.Find(shadowObjectName);
+        if (shadowObj != null)
+        {
+            shadowPlane = shadowObj.transform;
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (!dancePlayerCore.IsPlaying || shadowPlane == null)
+            return;
+        if (avatarHelper != null && hips == null)
+        {
+            var animator = avatarHelper.CurrentAnimator;
+            if (animator != null)
+            {
+                hips = animator.GetBoneTransform(HumanBodyBones.Hips);
+            }
+        }
+        if (hips == null)
+            return;
+        Vector3 pos = shadowPlane.position;
+
+        pos.x = hips.position.x;
+        pos.y = hips.position.y;
+
+        pos.z = hips.position.z + initialZOffset;
+
+        shadowPlane.position = pos;
+    }
+}
